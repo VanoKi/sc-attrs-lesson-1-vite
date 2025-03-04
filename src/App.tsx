302c4307ffb106652bbbed49b8a190c7d046ea0d
VanoKi <kirsvano@gmail.com>
2025-03-04 17:17:41 +0300

    feat: contacts

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
 import { Works } from './layout/sections/works/Works';
 import { Testimony } from './layout/sections/testimony/Testimony';
+import { Contacts } from './layout/sections/contacts/Contacts';
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Main />
-            <Skills/>
-            <Works/>
-            <Testimony/>
+            <Skills />
+            <Works />
+            <Testimony />
+            <Contacts />
         </div>
     );
 }
