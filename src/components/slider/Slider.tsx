974f9b5c80b0b3e816c2364123615e7dac32553f
VanoKi <kirsvano@gmail.com>
2025-03-04 16:45:16 +0300

    feat: Testimony

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+export const Slider = () => {
+    return (
+        <StyledSlider>
+            <Slide>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tempore nostrum eligendi quaerat exercitationem unde optio dicta et, ducimus labore fuga quis pariatur voluptatibus incidunt excepturi modi ratione ad nihil.</Text>
+                <Name>@Vasya Vasilevski</Name>
+            </Slide>
+        </StyledSlider>
+    )
+}
+
+const StyledSlider = styled.div`
+    
+`
+const Slide = styled.div`
+    
+`
+const Text = styled.p`
+    
+`
+const Name = styled.p`
+    
+`
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 import { SectionTitle } from "../../../components/SectionTitle";
 import { Icon } from "../../../components/icon/Icon";
+import { Slider } from "../../../components/slider/Slider";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <Icon iconId={'qioute'}/>
+            <Slider/>
         </StyledTestimony>
     );
 };
 
 const StyledTestimony = styled.section`
-
+    background-color: violet;
 `
 
