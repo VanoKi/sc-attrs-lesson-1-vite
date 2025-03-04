c3d626637983bfe0eea7115e1224551a92ee81a3
VanoKi <kirsvano@gmail.com>
2025-03-04 16:36:15 +0300

    feat: testimony

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Icon } from "../../../components/icon/Icon";
+
+export const Testimony = () => {
+    return (
+        <StyledTestimony>
+            <SectionTitle>Testimony</SectionTitle>
+            <Icon iconId={'qioute'}/>
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+
+`
+
