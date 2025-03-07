2d6368fb8a9fdc51812108d7c5e264d74f79d159
VanoKi <kirsvano@gmail.com>
2025-03-07 17:53:13 +0300

    fix: tag in the main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -31,7 +31,7 @@ const Photo = styled.img`
     object-fit: cover;
 `
 
-const StyledMain = styled.div`
+const StyledMain = styled.section`
     /* min-height: 100vh; */
     background-color: #f8f891;
 `
