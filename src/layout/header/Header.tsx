c41ce33fee0a5fe6bba8e364fd9f44791af1964e
VanoKi <kirsvano@gmail.com>
2025-03-07 18:04:27 +0300

    fix: add wrap to header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
+import {Contacts} from "../sections/contacts/Contacts.tsx";
+import {Container} from "../../components/Container.ts";
+import {FLexWrapper} from "../../components/FlexWrapper.tsx";
 
 const items = ['Home', 'Skils', 'Works', 'Testimony', 'Contact',]
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-            <Menu menuItems={items}/>
+            <Container>
+                <FLexWrapper justify={'space-between'} aline={'center'}>
+                    <Logo />
+                    <Menu menuItems={items}/>
+                </FLexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
