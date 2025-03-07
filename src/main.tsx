2071f674faaa9d968977e37a17957f4e58053638
VanoKi <kirsvano@gmail.com>
2025-03-07 17:46:22 +0300

    feat: add colors to Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {GlobalStyles} from "./styles/GlobalStyles.ts";
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-  <App />
+    <StrictMode>
+        <GlobalStyles />
+      <App />
+    </StrictMode>,
+    // <GlobalStyles/>
+    // <App/>
 )
