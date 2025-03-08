7eabf11f16157474a614848296db00935f1c6dbe
VanoKi <kirsvano@gmail.com>
2025-03-08 14:17:29 +0300

    feat: add pseudo elem to title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Theme } from "../styles/Theme";
 
 type SectionTitlePropsStyled = {
     direction?: string
@@ -7,6 +8,24 @@ type SectionTitlePropsStyled = {
     wrap?: string
 }
 export const SectionTitle = styled.h2<SectionTitlePropsStyled>`
-    color: #9b0202;
     text-align: center;
+    font-family: "Josefin Sans" sans-serif;
+    font-weight: 600;
+    font-size: 36px;
+    letter-spacing: 0.14em;
+
+    position: relative;
+
+    margin-bottom: 90px;
+    &::before {
+        content: '';
+        display: inline-block;
+        width: 55px;
+        height: 1px;
+        background-color: ${Theme.colors.accent};
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        transform: translateX(-50%);
+    }
 `
