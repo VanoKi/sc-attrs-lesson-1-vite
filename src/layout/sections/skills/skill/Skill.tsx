55ac3a29dbdc8079b4f56c1a618660617eb326e1
VanoKi <kirsvano@gmail.com>
2025-03-08 14:24:20 +0300

    feat: add wrapper to skill

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Icon } from "../../../../components/icon/Icon";
+import { FLexWrapper } from "../../../../components/FlexWrapper";
 
 type SkillPropsType = {
     iconId: string
@@ -9,9 +10,11 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
-            <Icon iconId={props.iconId}></Icon>
-            <SkillTitle>{props.title}</SkillTitle>
-            <SkillText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio tempore voluptatibus ab sapiente voluptate deserunt magnam omnis labore aperiam. Minima voluptatem expedita quia. Numquam natus autem blanditiis quam minima ea?</SkillText>
+            <FLexWrapper direction="column" aline="center">
+                <Icon iconId={props.iconId}></Icon>
+                <SkillTitle>{props.title}</SkillTitle>
+                <SkillText>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio tempore voluptatibus ab sapiente voluptate deserunt magnam omnis labore aperiam. Minima voluptatem expedita quia. Numquam natus autem blanditiis quam minima ea?</SkillText>
+            </FLexWrapper>
         </StyledSkill>
     );
 };
@@ -19,10 +22,6 @@ export const Skill = (props: SkillPropsType) => {
 const StyledSkill = styled.div`
     width: 380px;
     padding: 62px 20px 40px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
 `
 const SkillTitle = styled.h3`
     
