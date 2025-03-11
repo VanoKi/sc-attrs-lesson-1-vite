5a530c31da66476e02575fe1629815ed785b246b
VanoKi <kirsvano@gmail.com>
2025-03-11 17:32:18 +0300

    feat: add style to input

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,33 +1,43 @@
 import styled from "styled-components"
 import { SectionTitle } from "../../../components/SectionTitle"
 import { Button } from "../../../components/Button"
+import { Container } from "../../../components/Container"
+import { Theme } from "../../../styles/Theme"
 
 export const Contacts = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contacts</SectionTitle>
-            <StyledForm>
-                <Field placeholder={'name'}/>
-                <Field placeholder={'subject'}/>
-                <Field placeholder={'message'} as={'textarea'} />
-                <Button type={'submit'}>Send Message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contacts</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={'name'}/>
+                    <Field placeholder={'subject'}/>
+                    <Field placeholder={'message'} as={'textarea'} />
+                    <Button type={'submit'}>Send Message</Button>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     )
 }
 
 const StyledContacts = styled.section`
-    /* min-height: 50vh; */
-    background-color: #f1fdd6;
 `
 const StyledForm = styled.form`
-    max-width: 500px;
+    max-width: 540px;
     width: 100%;
     display: flex;
     flex-direction: column;
+    align-items: center;
     gap: 10px;
     margin: 0 auto;
 `
 const Field = styled.input`
-    
+    width: 100%;
+    background: ${Theme.colors.secondaryBg};
+    border: 1px solid #4a4a4a;
+    padding: 7px 15px;
+    font-weight: 400;
+    font-size: 12px;
+    letter-spacing: 0.05em;
+    color: #495057;
 `
