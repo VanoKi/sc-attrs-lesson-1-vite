17365314fcc7b766a955decdd58086b7ed834343
VanoKi <kirsvano@gmail.com>
2025-03-14 20:46:08 +0300

    feat: create a new Comon.tsx

---
+++ src/styles/Common.tsx
@@ -0,0 +1,17 @@
+import {Theme} from "./Theme.ts";
+
+type FontPropsType = {
+    family?: string
+    weight?: number
+    color?: string
+    lineHeight?: number
+    Fmin: number
+    Fmax: number
+}
+export const font = ({family, weight, color, lineHeight}: FontPropsType, Fmin, Fmax) => `
+    font-family: ${family || 'Poppins'};
+    font-weight: ${weight || 400};
+    color: ${color || Theme.colors.font};
+    lie-height: ${lineHeight || 1.2};
+    font-size: calc( (100vw - 360px)/(1440 - 360) * (${Fmax} -${Fmin}) + ${Fmin}px );
+`
