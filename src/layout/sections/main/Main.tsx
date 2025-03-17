807284cdc17dd43c009a2c8e801651d8e61ebff9
VanoKi <kirsvano@gmail.com>
2025-03-17 16:32:17 +0300

    feat: add mixin to main title

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -46,8 +46,9 @@ const StyledMain = styled.section`
     display: flex;
 `
 const MainTitle = styled.h1`
-    font-weight: 400;
-    font-size: 27px;
+    //font-weight: 400;
+    //font-size: 27px;
+    ${font({weight: 400,  Fmax: 27, Fmin: 20})}
 `
 const Name = styled.h2`
     ${font({family: "'Josefin Sans', sans-serif", weight: 700,  Fmax: 50, Fmin: 36})}
