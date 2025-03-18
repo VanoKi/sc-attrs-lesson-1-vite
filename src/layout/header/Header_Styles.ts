b8085ff18927141ad0b946e7410584ced69af2c0
VanoKi <kirsvano@gmail.com>
2025-03-18 17:04:45 +0300

    fix: export in header

--- src/layout/header/Header_Styles.ts
+++ src/layout/header/Header_Styles.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
-export const StyledHeader = styled.header`
+const Header = styled.header`
     background-color: #1F1F20;
     padding: 20px 0;
     position: fixed;
     top: 0;left: 0;right: 0;
     z-index: 99;
 `
+
+export const S = {
+    Header
+}
