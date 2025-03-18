41f2bdff7a286293a6669bbd004219a92adff620
VanoKi <kirsvano@gmail.com>
2025-03-18 17:02:15 +0300

    fix: move header styles to file

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,10 +1,10 @@
-import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import {Container} from "../../components/Container.tsx";
 import {FLexWrapper} from "../../components/FlexWrapper.tsx";
 import {HeaderMenu} from "./headerMenu.tsx";
 import {MobileMenu} from "../mobileHeader/mobileMenu.tsx"
 import React from "react";
+import {StyledHeader} from "./Header_Styles.ts";
 
 const items = ['Home', 'Skills', 'Works', 'Testimony', 'Contact',]
 export const Header: React.FC = () => {
@@ -21,10 +21,3 @@ export const Header: React.FC = () => {
     );
 };
 
-const StyledHeader = styled.header`
-    background-color: #1F1F20;
-    padding: 20px 0;
-    position: fixed;
-    top: 0;left: 0;right: 0;
-    z-index: 99;
-`
