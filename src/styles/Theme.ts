21e3e38766abab134cc22c20cdbd828a752319cb
VanoKi <kirsvano@gmail.com>
2025-03-19 14:56:25 +0300

    feat: add bg

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,8 +1,9 @@
 export const Theme = {
     colors: {
-        primaryBg: "#1F1F20",
-        secondaryBg: "#252527",
-        accent: "#7572D5",
+        primaryBg: "#222A36",
+        secondaryBg: "#000000",
+        thirdBg: "#222A36",
+        accent: "#676CDB",
         font: "#ffffff",
         borderColor: '#4a4a4a',
         placeholderColor: '#495057'
