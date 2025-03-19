deaa383b3c79892e0f46d490c6ca78c5b1a983ab
VanoKi <kirsvano@gmail.com>
2025-03-19 15:34:06 +0300

    fix: del tags in mark up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import './App.css';
-import { Header } from './layout/header/Header';
-import { Main } from './layout/sections/main/Main';
-import { Skills } from './layout/sections/skills/Skills';
-import { Works } from './layout/sections/works/Works';
-import { Contacts } from './layout/sections/contacts/Contacts';
-import { Footer } from './layout/sections/footer/Footer';
-import {Testimony} from "./layout/sections/testimony/Testimony.tsx";
+
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Main />
-            <Skills />
-            <Works />
-            <Testimony />
-            <Contacts />
-            <Footer />
+
         </div>
     );
 }
