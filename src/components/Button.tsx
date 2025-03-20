ee4e97390cc85fa58f69d9f58260dcc43d35310a
VanoKi <kirsvano@gmail.com>
2025-03-20 20:26:47 +0300

    feat: add transition

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import { Theme } from "../styles/Theme";
 
 export const Button = styled.button`
     background: transparent;
+    transition: background-color 0.3s ease;
     &:hover {
         background: ${Theme.colors.accent}
     }
